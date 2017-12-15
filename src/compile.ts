411a96d878a712c0ca3db4c0b552f6a39456526e
Brad Davies <varbrad@users.noreply.github.com>
2017-12-15 18:42:00 +0000

    Compiler now allows any number representation
    e.g. `set 0xFF`, `set 255`, `set 0b11111111` are all equivalent now

--- src/compile.ts
+++ src/compile.ts
@@ -3,9 +3,10 @@ import Memory from './storage/Memory'
 import { Oplabel, getOpcode } from './ops'
 
 function getLabelValue(v: string): number {
+  if (v === '') throw new Error(`Non-existant label value`)
   const indexOf = 'abcdefgh'.indexOf(v)
   if (indexOf > -1) return indexOf + 32768
-  let value: number = parseInt(v)
+  let value: number = Number(v)
   if (isNaN(value)) throw new Error(`Error parsing value "${v}"`)
   if (value < 0 || value > 32775)
     throw new Error(`Out-of-range value "${value}"`)
