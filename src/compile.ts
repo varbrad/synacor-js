d0007d43f68a6d16d6e89e9a20a433d2ac54de17
Brad Davies <varbrad@users.noreply.github.com>
2017-12-15 17:43:43 +0000

    Initial compiler implementation

--- src/compile.ts
+++ src/compile.ts
@@ -1,3 +1,36 @@
-function compile(data: number) {
-  return 123
+import Memory from './storage/Memory'
+
+import { Oplabel, getOpcode } from './ops'
+
+function getLabelValue(v: string): number {
+  const indexOf = 'abcdefgh'.indexOf(v)
+  if (indexOf > -1) return indexOf + 32768
+  let value: number = parseInt(v)
+  if (isNaN(value)) throw new Error(`Error parsing value "${v}"`)
+  if (value < 0 || value > 32775)
+    throw new Error(`Out-of-range value "${value}"`)
+  return value
+}
+
+function flattenArray(array: any[]): any[] {
+  return array.reduce(
+    (flat, toFlatten) =>
+      flat.concat(
+        Array.isArray(toFlatten) ? flattenArray(toFlatten) : toFlatten
+      ),
+    []
+  )
+}
+
+export function compile(source: string): Memory {
+  const data = source
+    .trim()
+    .split('\n')
+    .map(v => v.split(' '))
+    .map((line, ln) => {
+      const opcode = getOpcode(line[0] as Oplabel)
+      const args = line.slice(1).map(getLabelValue)
+      return [opcode, ...args]
+    })
+  return Memory.fromArray(flattenArray(data))
 }
